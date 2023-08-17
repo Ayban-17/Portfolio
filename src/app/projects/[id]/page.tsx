2a0293f0fab1555fab023fa87990dc97a69a0c44
Ayban <mrkvnhrnndz17@gmail.com>
2023-08-17 19:17:10 +0800

    Changed the flex to 80%

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,5 +1,5 @@
 const Project = ({ params }: any) => {
-  return <div className="text-white lg:flex-[80%]">Project {params.id}</div>;
+  return <div className="text-white flex-[80%]">Project {params.id}</div>;
 };
 
 export default Project;
